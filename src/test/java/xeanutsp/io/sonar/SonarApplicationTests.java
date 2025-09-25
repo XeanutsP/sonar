c575fb7d843f9dd8a5a2e0107868f868b1586303
XeanutsP <darkeningxenos@gmail.com>
2025-09-25 20:30:35 +0700

    add test and dockerfile

--- src/test/java/xeanutsp/io/sonar/SonarApplicationTests.java
+++ src/test/java/xeanutsp/io/sonar/SonarApplicationTests.java
@@ -1,13 +1,32 @@
 package xeanutsp.io.sonar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import xeanutsp.io.sonar.repo.UsersRepository;
+import xeanutsp.io.sonar.service.UsersService;
 
 @SpringBootTest
 class SonarApplicationTests {
 
+	@Mock
+	private UsersRepository usersRepository;
+
+	@InjectMocks
+	private UsersService usersService;
+
+	@BeforeEach
+	void setup(){
+		MockitoAnnotations.openMocks(this);
+	}
+
 	@Test
-	void contextLoads() {
+	void testGetUser() {
+
 	}
 
 }
