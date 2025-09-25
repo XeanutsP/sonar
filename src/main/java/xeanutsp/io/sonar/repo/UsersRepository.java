d35257d4f97a87208082103d4faf080f88aff915
XeanutsP <darkeningxenos@gmail.com>
2025-09-25 20:14:10 +0700

    Update

--- src/main/java/xeanutsp/io/sonar/repo/UsersRepository.java
+++ src/main/java/xeanutsp/io/sonar/repo/UsersRepository.java
@@ -11,12 +11,16 @@ public interface UsersRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT * FROM User u")
     List<User> findAll();
-    
+
     @Query(value = "SELECCT * FROM User u WHERE id = ?1")
     List<User> find(Long id);
 
-    @Query()
+    @Query(value = "INSERT INTO User u(id, name, username,email, phone, webiste) VALUES ?1")
     User saveUser(User userAdd);
+
+    @Query(value = "UPDATE TABLE User u WHERE u = ?1")
     User updateUser(Long id, User userUpdate);
+
+    @Query(value = "DELETE FROM User u WHERE u.id = ?1")
     void deleteById(Long id);
 }
